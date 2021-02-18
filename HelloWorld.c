c3fcb34a7b0dfc7f563fd2b34303f426af330174
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-02-19 00:04:33 +0900

    first commit

---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Cのコードはmain関数（システムが最初に呼び出す関数）の中に書く
+// main関数は戻り値として0を返すのでint型で定義する
+int main() {
+  // 標準出力で何かを出力するためのprintf関数
+  printf("Hello World\n");
+
+  // main関数の最後は正常に終了したことを通知するために0を返す
+  return 0;
+}
